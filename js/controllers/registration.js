1c72c5db0b82dfa4b8d6a40bc7cfc007b3c6e3d9
Nathan Gaini <nathan.gaini3@gmail.com>
2016-08-22 00:33:13 -0700

    completed module 1

---
+++ js/controllers/registration.js
@@ -0,0 +1,13 @@
+myApp.controller('RegistrationController',['$scope', function($scope){
+
+    $scope.login= function(){
+        $scope.message = "Welcome "+ $scope.user.email;
+    };
+
+    $scope.register= function(){
+        $scope.message = "Welcome "+ $scope.user.fname;
+    };
+    // $scope.message = "Welcome to my app";
+
+
+}]);
---
+++ js/controllers/success.js
@@ -0,0 +1,4 @@
+myApp.controller('SuccessController',['$scope', function($scope){
+
+    $scope.message = "Welcome to my app";
+}]);
